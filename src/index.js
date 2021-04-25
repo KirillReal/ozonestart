ed69632a8bddd45eac9077c2fc3f9009589207cc
KirillReal <kiril5471710@gmail.com>
2021-04-25 16:24:52 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+import getData from '.modules/getData';
+import renderCards from '.modules/renderCards';
+import renderCatalog from '.modules/renderCatalog';
+import toggleCheckbox from '.modules/toggleCheckbox';
+import toggleCart from '.modules/toggleCart';
+import addCart from '.modules/addCart';
+import actionPage from '.modules/actionPage';
+
+
+getData().then((data) =>{
+    renderCards(data);
+    toggleCheckbox();
+    toggleCart();
+    addCart();
+    actionPage();
+    renderCatalog();
+}
+);
+
+
